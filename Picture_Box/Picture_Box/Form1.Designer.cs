82a8680034b622e447444fb97f3b20104f28f05a
Avdicci <119886112+Avdicci@users.noreply.github.com>
2023-03-27 11:10:47 +0200

    Add files via upload
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Picture_Box/Picture_Box/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Picture_Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pic_mid = new System.Windows.Forms.PictureBox();
+            this.pic_left = new System.Windows.Forms.PictureBox();
+            this.pic_right = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_mid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_left)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_right)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click me to choose a pic";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Picture_Box.Properties.Resources.Skärmklipp1;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(222, 179);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Picture_Box.Properties.Resources.islam_nature_quran_wallpaper_preview;
+            this.pictureBox3.Location = new System.Drawing.Point(566, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(222, 179);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Picture_Box.Properties.Resources.Skärmbild_2022_12_15_125051;
+            this.pictureBox2.Location = new System.Drawing.Point(240, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 179);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pic_mid
+            // 
+            this.pic_mid.Location = new System.Drawing.Point(290, 197);
+            this.pic_mid.Name = "pic_mid";
+            this.pic_mid.Size = new System.Drawing.Size(222, 170);
+            this.pic_mid.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_mid.TabIndex = 0;
+            this.pic_mid.TabStop = false;
+            // 
+            // pic_left
+            // 
+            this.pic_left.Location = new System.Drawing.Point(12, 197);
+            this.pic_left.Name = "pic_left";
+            this.pic_left.Size = new System.Drawing.Size(272, 170);
+            this.pic_left.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_left.TabIndex = 5;
+            this.pic_left.TabStop = false;
+            // 
+            // pic_right
+            // 
+            this.pic_right.Location = new System.Drawing.Point(518, 197);
+            this.pic_right.Name = "pic_right";
+            this.pic_right.Size = new System.Drawing.Size(272, 170);
+            this.pic_right.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_right.TabIndex = 6;
+            this.pic_right.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(272, 65);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Click me for a pic";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(516, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(272, 65);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Click me for a pic";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pic_right);
+            this.Controls.Add(this.pic_left);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic_mid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_mid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_left)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_right)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_mid;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pic_left;
+        private System.Windows.Forms.PictureBox pic_right;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Picture_Box/Picture_Box/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Image = Image.FromFile(@"c:\picture.png");
+            openFileDialog1.ShowDialog();
+            string filepath = openFileDialog1.FileName;
+            pic_mid.Image = Image.FromFile(filepath);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pic_left.Image = Image.FromFile(@"c:\picture.png");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pic_right.Image = Image.FromFile(@"c:\picture.png");
+        }
+    }
+}
